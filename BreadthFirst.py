76203913c39b48f2b1c432a65c5e7805e3caeca8
Augusto Castilho <castilho@ice.ufjf.br>
2022-12-04 20:08:27 -0300

    Criacao dos arquivos para busca

---
+++ BreadthFirst.py
@@ -0,0 +1,9 @@
+from Search import Search
+
+
+# Busca em largura
+class BreadthFirst(Search):
+
+    def search(self):
+        while self.__success == False and self.__fail == False:
+            pass
---
+++ DepthFirst.py
@@ -0,0 +1,9 @@
+from Search import Search
+
+
+# Busca em profundidade
+class DepthFirst(Search):
+
+    def search(self):
+        while self.__success == False and self.__fail == False:
+            pass
